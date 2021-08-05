e343c35d793cc5d6365ac28272b3a83ddca79d47
bhsoham <sohambhattacharjee0711@gmail.com>
2021-08-05 21:34:05 +0530

    initial commit

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Loading from "../components/Loading";
+import "../css/landing.css";
 
 const LandingPage = () => {
-  return <div className="btn btn-primary">sansas</div>;
+  return <div className="btn btn-primary">kansas</div>;
+          
 };
+
 
 export default LandingPage;
