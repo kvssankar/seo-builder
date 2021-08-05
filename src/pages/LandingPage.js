ea664b130d88e335f66bcdd07048d6a99cd686d3
bhsoham <sohambhattacharjee0711@gmail.com>
2021-08-05 23:50:19 +0530

    minor fixes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
         <div class="col-5 hero-img">
           <h1>SEO Builder</h1>
         </div>
-        <div class="col-7">
+        <div class="col-7 hero-text">
         Use our powerful mobile-first flexbox grid to build layouts of all shapes and sizes thanks to a twelve column system, five default responsive tiers, Sass variables and mixins, and dozens of predefined classes.
         </div>
       </div>
