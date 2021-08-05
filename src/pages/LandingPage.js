49d2ca85b4ad6e986a79281f9e314efa47280185
bhsoham <sohambhattacharjee0711@gmail.com>
2021-08-05 23:41:10 +0530

    made section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,8 +3,18 @@ import Loading from "../components/Loading";
 import "../css/landing.css";
 
 const LandingPage = () => {
-  return <div className="btn btn-primary">kansas</div>;
-          
+  return (
+    <div className="container-fluid">
+      <div class="row">
+        <div class="col-5 hero-img">
+          <h1>SEO Builder</h1>
+        </div>
+        <div class="col-7">
+        Use our powerful mobile-first flexbox grid to build layouts of all shapes and sizes thanks to a twelve column system, five default responsive tiers, Sass variables and mixins, and dozens of predefined classes.
+        </div>
+      </div>
+    </div>
+  );   
 };
 
 
