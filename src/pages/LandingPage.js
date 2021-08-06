82ad8eb45474c09868ac71bd0656080f75a806f0
bhsoham <sohambhattacharjee0711@gmail.com>
2021-08-06 17:47:48 +0530

    basic built

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,12 +5,35 @@ import "../css/landing.css";
 const LandingPage = () => {
   return (
     <div className="container-fluid">
-      <div class="row">
-        <div class="col-5 hero-img">
+      <div className="row">
+        <div className="col-4 hero-art">
           <h1>SEO Builder</h1>
+          <h2>Your one-stop destination for everything</h2>
         </div>
-        <div class="col-7 hero-text">
-        Use our powerful mobile-first flexbox grid to build layouts of all shapes and sizes thanks to a twelve column system, five default responsive tiers, Sass variables and mixins, and dozens of predefined classes.
+        <div className="col-8 hero-text">
+          <div className="sign-up">
+            Not a member? <a href="">Sign up now</a>
+          </div>
+          <div className="main">
+            <h1>Sign in to SEO Builder</h1>
+            <button className= "btn" type="submit"><img className="icon-google" src="../imgs/google.png" alt="icon"/>   Sign in with Google</button>
+            <hr className="divider"/>
+            <form>
+              <div className="login-form">
+                <label for="login">Username or Email Address</label><br/>
+                <input type="text" name="login" id="login"></input><br/>
+
+                <label for="password">Password <a href="/">Forgot password</a></label><br/>
+                <input type="password" name="password" id="password"></input><br/>
+
+              </div>
+
+            </form>
+            
+            <button className= "btn btn-signin" type="submit">Sign in</button>
+
+          
+          </div>
         </div>
       </div>
     </div>
