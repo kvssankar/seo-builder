70737fa87a3fe9bf33437de6cce41c2544e9291f
bhsoham <sohambhattacharjee0711@gmail.com>
2021-08-09 22:10:26 +0530

    initial commit: basic struct

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Loading from "../components/Loading";
-import "../css/landing.css";
+import "../css/firebase.css";
 
 const LandingPage = () => {
   return (
@@ -14,8 +14,46 @@ const LandingPage = () => {
           {/* <img class="hero-img" src="https://cdn.dribbble.com/assets/auth/sign-in-a63d9cf6c1f626ccbde669c582b10457b07523adb58c2a4b46833b7b4925d9a3.jpg"/> */}
           <img class="hero-img" src="../imgs/undraw_websiteBuilder.svg"/>
         </div>
-        <div className="col-8 hero-text">
-          <div className="sign-up">
+        <div className="col-8 hero-text"> 
+          <div className="firebase">
+            <h2>Firebase Admin SDK</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque, iusto ut, incidunt mollitia eius nisi repudiandae enim, rerum assumenda voluptatibus blanditiis omnis aut magnam voluptatem cum praesentium. Nisi, voluptate? <a href="/">Learn more</a></p>
+            <p>Firebase service accound</p>
+            <strong>seo-builder@seobuilder.com</strong>
+            <hr/>
+            <p>Admin SDK configuration snippet</p>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+              <label class="form-check-label" for="inlineRadio1">Node.js</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+              <label class="form-check-label" for="inlineRadio2">Java</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+              <label class="form-check-label" for="inlineRadio2">Python</label>
+            </div>
+            <hr/>
+
+            <button className= "btn" type="submit">GENERATE NEW PRIVATE KEY</button>
+
+
+          </div>
+          
+
+
+
+
+
+
+
+
+
+
+
+
+          {/* <div className="sign-up">
             Not a member? <a href="">Sign up now</a>
           </div>
           <div className="main">
@@ -37,7 +75,7 @@ const LandingPage = () => {
             <button className= "btn btn-signin" type="submit">Sign in</button>
 
           
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
